5e6853719795343881c79829ac454a2698c09eb1
TheOfficialAyush <ayushanand8171@gmail.com>
2021-05-15 20:05:48 +0530

    disconnect 2

--- server.js
+++ server.js
@@ -24,5 +24,17 @@ client.on("message", message => {
         .catch(err => console.log(err));
     })();
   }
+  function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could stop!");
+      
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  }
 });
 client.login(process.env.token);

1540b92b2e753c69924b8d0e4c06dae04fbebb79
TheOfficialAyush <ayushanand8171@gmail.com>
2021-05-16 15:52:03 +0530

    rain

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 client.on("ready", function() {
   console.log("Connected");
 });
 client.on('ready', async () => {
- client.user.setActivity("24/7 | TheOfficialAyush#0001",{type: "LISTENING"})
-    }); 
+  client.user.setActivity("24/7 | TheOfficialAyush#0001",{type: "LISTENING"})
+     }); 
 client.on("message", message => {
   if (message.content.startsWith(`<@!${client.user.id}>`)) {
     (async () => {
       var voiceChannel = message.member.voice.channel;
       voiceChannel
         .join()
-          .then(async connection => {
+        .then(async connection => {
           const dispatcher = connection.play(
-            "https://rainymood.com/audio1112/0.m4a"
+            "https://ice55.securenetsystems.net/DASH45"
           );
-          
           dispatcher.on("finish", end => {
             voiceChannel.leave();
           });
@@ -25,4 +25,5 @@ client.on("message", message => {
     })();
   }
 });
+
 client.login(process.env.token);
